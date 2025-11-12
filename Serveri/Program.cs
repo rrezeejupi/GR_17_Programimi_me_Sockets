539cddf1c63d4637d9a7e238b6f41eb14bcb1328
Yllka <yllka.fejzullahu@student.uni-pr.edu>
2025-11-12 19:31:53 +0100

    Shtimi i TCP Serverit qe pret/ndegjon per klient connections

---
+++ Serveri/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPServer
+{
+    static void Main(string[] args)
+    {
+        TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 6789);
+        listener.Start();
+        Console.WriteLine("Serveri po degjon ne portin 6789...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Klient i ri u lidh!");
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Mesazhi nga klienti: " + message);
+
+            string response = "Echo: " + message;
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseData, 0, responseData.Length);
+
+            client.Close();
+        }
+    }
+}
